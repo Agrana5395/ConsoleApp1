e937856a48612e562e3e649f7114c958dc6729f4
Alfredo Granado <alfred_grand@outlook.com>
2017-03-18 20:52:04 -0400

    the program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //HelloWorld();
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("What is your city: ");
+            string city = Console.ReadLine();
+
+            string reversedName = ReverseString(name);
+            string reversedCity = ReverseString(city);
+
+            ReverseString(name);
+            ReverseString(city);
+            Console.ReadLine();
+        }
+
+        //private static void HelloWorld()
+        //{
+        //    Console.WriteLine("Hello World");
+        //}
+
+        private static string ReverseString( string message )
+        {
+            
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            return String.Concat(messageArray);
+           
         }
     }
 }
